b11d1efe346dfa1ce601444d0fcd0fbe77813d10
Fall Xavier <84975017+Fall-Xavier@users.noreply.github.com>
2025-03-02 21:30:23 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,9 +3,15 @@ import cors from "cors"
 import router from "../src/routes/router.js"
 
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
+
 app.use("/api", router)
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
 
 export default app
